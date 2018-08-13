4a94c9c13ff04df0824510dd5eb4ac2a894c114c
Colin <colingoodale@gmail.com>
2018-08-12 21:15:00 -0500

    almost done with timer

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,36 +2,49 @@ var questions = [
     {
         question: "What is the capital of Illinois?",
         options: ["Chicago", "Peoria", "Springfield", "Dekalb"],
-        answer: 2,
+        answer: "a2",
     },
     {
         question: "How many states are there in the United States of America?",
         options: ["51", "52", "48", "50"],
-        answer: 3,
+        answer: "a3",
     },
     {
         question: "Who is the main character of Harry Potter?",
         options: ["Hermione", "Harry", "Ron", "Ginny"],
-        answer: 1,
+        answer: "a1",
     },
     {
         question: "What does a HuffleUpAGuss resemble?",
         options: ["Adulthood", "Elephants", "A Rumple and A Rample", "All of the Above"],
-        answer: 3,
-    }
+        answer: "a3",
+    },
+    {
+        question: "What statue was gifted to the United States by the French?",
+        options: ["The Statue of Liberty", "Eiphel Tower", "Tower of Babble", "Trump Mar-A-Lago"],
+        anwer: "a0",
+    },
+    {
+        questions: "Wherfore art Thou?",
+        options: ["to the East", "to the West", "to the South", "to the North"],
+        answer: "a2",
+    },
+    {
+        questions: "How difficult is this assignment?",
+        options: ["frustratin", "simple", "promising", "Ooga La Boonga"],
+        answer: "a3",
+    },
 ]
 //choose random object from array
 //var chosenQuestion = questions[Math.floor(Math.random() * questions.length)];
 // create start function
 var gotQuestion = false;
+var playerScore = 0;
 
 //create function to choose quesiton with onclick event.
-function chooseQuestion() {
-    var chosenQuestion = questions[Math.floor(Math.random() * questions.length)];
-    console.log(chosenQuestion);
-}
+//moved to onClick
 
-chooseQuestion();
+
 
 
 
